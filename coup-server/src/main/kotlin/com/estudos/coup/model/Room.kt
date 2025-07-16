b916d1eaaa52e13dc209f7eb3c057dc235899653
jvaraujo-dev <j.vitorsilva.dev@gmail.com>
2025-07-16 12:28:56 -0300

    fix cascade parameter

--- coup-server/src/main/kotlin/com/estudos/coup/model/Room.kt
+++ coup-server/src/main/kotlin/com/estudos/coup/model/Room.kt
@@ -6,7 +6,6 @@ import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
 import jakarta.persistence.Id
-import jakarta.persistence.JoinColumn
 import jakarta.persistence.OneToMany
 import java.util.UUID
 
@@ -17,7 +16,7 @@ data class Room(
     val token: String = UUID.randomUUID().toString(),
     val roomName: String,
 
-    @OneToMany(mappedBy = "room",fetch = FetchType.EAGER, cascade = CascadeType.PERSIST)
+    @OneToMany(mappedBy = "room",fetch = FetchType.EAGER, cascade = [CascadeType.PERSIST])
     var player: MutableList<Player> = mutableListOf()
 ){
     constructor() : this(
