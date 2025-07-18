8209b40fa4cd067d89e02ed1ac8ab39cb65392b6
jvaraujo-dev <j.vitorsilva.dev@gmail.com>
2025-07-18 03:17:54 -0300

    Add screen for creation room and fix docker integration

--- coup-server/src/main/kotlin/com/estudos/coup/controller/MatchmakingController.kt
+++ coup-server/src/main/kotlin/com/estudos/coup/controller/MatchmakingController.kt
@@ -5,18 +5,20 @@ import com.estudos.coup.controller.response.RoomResponse
 import com.estudos.coup.model.toRoomResponse
 import com.estudos.coup.services.MatchService
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
+@CrossOrigin(origins = ["http://localhost:3000"])
 class MatchmakingController(
     private val matchService: MatchService
 ) {
     @PostMapping("/create-room")
     @ResponseStatus(HttpStatus.CREATED)
-    fun createRoom(@RequestBody roomParameters: RoomRequest): RoomResponse{
+    fun createRoom(@RequestBody roomParameters: RoomRequest): RoomResponse {
         val roomResponse = matchService.createRoom(roomName = roomParameters.roomName).toRoomResponse();
         return roomResponse;
     }
