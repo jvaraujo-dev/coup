7a1e6562b55b9f1cebd3782220f8c52bc1afacfb
jvaraujo-dev <j.vitorsilva.dev@gmail.com>
2025-09-13 01:37:34 -0300

    add missing environment variable

--- coup-server/src/main/kotlin/com/estudos/coup/controller/MatchmakingController.kt
+++ coup-server/src/main/kotlin/com/estudos/coup/controller/MatchmakingController.kt
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@CrossOrigin(origins = ["http://localhost:3000"])
+@CrossOrigin(origins = [$$"${ALLOWED_ORIGINS}"])
 class MatchmakingController(
     private val matchService: MatchService
 ) {
